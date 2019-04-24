29924e5ab0e19b8378dec1dbb665a9d34f5b698e
RDoney <theryandoney@gmail.com>
2019-04-24 11:00:32 -0500

    removed cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/Ryan/Documents/gatsby-bulma/Fishcrazy-website/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"UA-XXXXXXXX-X","anonymize":true},
-    },{
-      plugin: require('/Users/Ryan/Documents/gatsby-bulma/Fishcrazy-website/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
